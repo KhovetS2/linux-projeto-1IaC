9f1d57da303118eddbe2317a3c3a338291a00a81
Isaque <isaque.elis.escola@gmail.com>
2023-07-21 17:35:58 -0300

    script para deletar estrutura criada

---
+++ scriptDell.sh
@@ -0,0 +1,18 @@
+rm -r -f /publico
+rm -r -f /adm
+rm -r -f /ven
+rm -r -f /sec
+
+userdel -r -f carlos
+userdel -r -f maria
+userdel -r -f joao
+userdel -r -f debora
+userdel -r -f sebastiana
+userdel -r -f roberto
+userdel -r -f josefina
+userdel -r -f amanda
+userdel -r -f rogerio
+
+groupdel GRP_ADM
+groupdel GRP_VEN
+groupdel GRP_SEC
