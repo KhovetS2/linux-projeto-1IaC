ac8f205e387724d6a5f5247f4d81d3970f366428
Isaque <isaque.elis.escola@gmail.com>
2023-07-21 17:25:00 -0300

    script do desafio

---
+++ script.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "Criando Diretórios ..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando Grupos..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando Usuários..."
+
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+passwd carlos -e
+
+useradd maria -c "Maria" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+passwd maria -e
+
+useradd joao -c "Joao" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+passwd joao -e
+
+useradd debora -c "Debora" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+passwd debora -e
+
+useradd sebastiana -c "Sebastiana" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+passwd sebastiana -e
+
+useradd roberto -c "Roberto" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+passwd roberto -e
+
+useradd josefina -c "Josefina" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+passwd josefina -e
+
+useradd amanda -c "Amanda" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+passwd amanda -e
+
+useradd rogerio -c "Rogerio" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+passwd rogerio -e
+
+
+echo "Configurando permissões..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
